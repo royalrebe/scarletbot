f06a405752043564c66a03df6552b2a70a6da717
royalrebe <108943414+royalrebe@users.noreply.github.com>
2022-08-10 05:31:22 -0500

    Add files via upload

---
+++ Avenger/helper/custom_filter.py
@@ -0,0 +1,94 @@
+import re
+from typing import List, Union
+
+from pyrogram.filters import create
+from pyrogram.types import Message
+from Avenger import BOT_USERNAME
+
+PREFIX = "/"
+# Global variables for dmins commands, connection admin commands & connection user commands 
+DISABLE_COMMANDS = []
+COMMANDS_LIST = []
+
+def command_lister(commands: Union[str, List[str]], disable: bool = False) -> list:
+    if isinstance(commands, str):
+        if disable:
+            DISABLE_COMMANDS.append(commands)
+
+    if isinstance(commands, list):
+        if disable:
+            for command in commands:
+                DISABLE_COMMANDS.append(command)
+
+def commandsHelper(commands: Union[str, List[str]]) -> list:
+    if isinstance(commands, str):
+        username_command = f"{commands}@{BOT_USERNAME}"
+        COMMANDS_LIST.append(commands)
+        COMMANDS_LIST.append(username_command)
+
+    if isinstance(commands, list):
+        for command in commands:
+            username_command = f"{command}@{BOT_USERNAME}"
+            COMMANDS_LIST.append(command)
+            COMMANDS_LIST.append(username_command)
+    
+    return COMMANDS_LIST
+
+
+def command(
+    commands: Union[str, List[str]],
+    prefixes: Union[str, List[str]] = PREFIX,
+    case_sensitive: bool = False,
+    disable: bool = False,
+    ):
+    
+    command_lister(commands, disable)
+    commands = commandsHelper(commands)
+    
+    command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
+    async def func(flt, _, message: Message):
+        text = message.text or message.caption
+        message.command = None
+
+        if not text:
+            return False
+
+        pattern = r"^{}(?:\s|$)" if flt.case_sensitive else r"(?i)^{}(?:\s|$)"
+
+        for prefix in flt.prefixes:
+            if not text.startswith(prefix):
+                continue
+
+            without_prefix = text[len(prefix):]
+
+            for cmd in flt.commands:
+                if not re.match(pattern.format(re.escape(cmd)), without_prefix):
+                    continue
+
+                # match.groups are 1-indexed, group(1) is the quote, group(2) is the text
+                # between the quotes, group(3) is unquoted, whitespace-split text
+
+                # Remove the escape character from the arguments
+                message.command = [cmd] + [
+                    re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
+                    for m in command_re.finditer(without_prefix[len(cmd):])
+                ]
+
+                return True
+
+        return False
+
+    commands = commands if isinstance(commands, list) else [commands]
+    commands = {c if case_sensitive else c.lower() for c in commands}
+
+    prefixes = [] if prefixes is None else prefixes
+    prefixes = prefixes if isinstance(prefixes, list) else [prefixes]
+    prefixes = set(prefixes) if prefixes else {""}
+
+    return create(
+        func,
+        "CommandFilter",
+        commands=commands,
+        prefixes=prefixes,
+        case_sensitive=case_sensitive
+    )
---
+++ Avenger/helper/get_user.py
@@ -0,0 +1,84 @@
+from Avenger import pbot
+
+async def get_user_id(message):
+    if(
+        message.reply_to_message
+        and not message.forward_from
+    ):
+        if (
+            len(message.command) >= 2
+        ):
+            args = message.command[1]
+            if (
+                args.startswith('@')
+                or (
+                    args.isdigit()
+                    and (
+                        len(args) >= 5
+                        or len(args) <=15
+                    )
+                )
+            ):
+                user_info = await pbot.get_users(
+                    user_ids=args
+                )
+                return user_info
+            else:
+                user_info = message.reply_to_message.from_user
+                return user_info
+        else:
+            user_info = message.reply_to_message.from_user
+            return user_info 
+
+    elif (
+        message.forward_from
+    ):
+        user_info = message.forward_from
+        return user_info 
+
+    elif not (
+        message.reply_to_message
+        or message.forward_from
+    ):
+        if not (
+            len(message.command) >= 2
+        ):
+            await message.reply(
+                "I don't know who you're talking about, you're going to need to specify a user...!"
+            )
+            return False
+            
+        user = message.command[1]
+        user_info = await pbot.get_users(
+            user_ids=user
+        )
+        
+        return user_info  
+
+def get_text(message):
+    if( 
+        message.reply_to_message
+    ):
+        if (
+            len(message.command) >= 2
+            and (
+                message.command[1].startswith('@')
+                or (
+                        message.command[1].isdigit()
+                        and (
+                            len(message.command[1]) >= 5
+                            or len(message.command[1]) <=15
+                        )
+                )
+            )
+        ):
+            text = ' '.join(message.command[2:])
+        else:
+            text = ' '.join(message.command[1:])
+    
+    elif (
+        not message.reply_to_message
+    ):
+        text = ' '.join(message.command[2:])
+    
+    return text
